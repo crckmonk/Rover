c0332acf9c48563fc1780aa7c7e86f1afa7dbbb3
Drew <drew@draxphoto.com>
2026-02-23 04:23:25 +0200

    Rewriting UART logic; Adding ring buffers for RX TX; WIP

--- Inc/usart.h
+++ Inc/usart.h
@@ -29,20 +29,39 @@ extern "C" {
 #include "main.h"
 
 /* USER CODE BEGIN Includes */
-
+#define UART_TIMEOUT 1000
 /* USER CODE END Includes */
 
 extern UART_HandleTypeDef huart1;
 
+extern UART_HandleTypeDef huart6;
+
 /* USER CODE BEGIN Private defines */
 
+
+#define UART_BUFFER_SIZE 256
+
+
+typedef struct {
+     UART_HandleTypeDef* huart;
+     uint8_t TxBuffer[UART_BUFFER_SIZE];
+     uint16_t TxWrite;
+     uint16_t TxRead;
+     uint8_t TxBusy;
+     uint8_t RxBuffer[UART_BUFFER_SIZE];
+     uint16_t RxHead;
+     uint16_t RxTail;
+     uint8_t RxByte;  // Single byte for HAL_UART_Receive_IT
+} UART_Buffers_t;
 /* USER CODE END Private defines */
 
 void MX_USART1_UART_Init(void);
+void MX_USART6_UART_Init(void);
 
 /* USER CODE BEGIN Prototypes */
 void _putchar(char character);
-
+uint8_t UART_SendByte(volatile UART_Buffers_t* uart, uint8_t byte);
+uint8_t UART_SendData(volatile UART_Buffers_t* uart, uint8_t* data, uint32_t size);
 /* USER CODE END Prototypes */
 
 #ifdef __cplusplus
